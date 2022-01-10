80b405ca567690556f0ea4496a44542cdb627a42
Pasha006 <91893567+Yan3330@users.noreply.github.com>
2022-01-10 17:54:12 +0200

    abc

---
+++ main.js
@@ -0,0 +1,48 @@
+// let a = prompt('Длина стороны квадрата: ')
+// let p = 4*a
+// alert(p)
+
+// let a = prompt('Длина стороны квадрата: ')
+// let s = a**2
+// alert(s)
+
+// let a = prompt('Длина первой стороны прямоугольника: ')
+// let b = prompt('Длина второй стороны прямоугольника: ')
+// let s = a*b
+// let p = 2*(a+b)
+// alert(`Площадь прямоугольника= ${s} Периметр прямоугольника= ${p}`)
+
+// let d = prompt('Диаметр окружности: ')
+// let pi = 3.14
+// let l = pi*d
+// alert(l)
+
+// let a = prompt('Длина ребра куба: ')
+// let v = a**3
+// let s = 6*a**2
+// alert(`Объем куба=${v} площадь его поверхности=${s}`)
+
+// let a = prompt('Длина ребра a: ')
+// let b = prompt('Длина ребра b: ')
+// let c = prompt('Длина ребра c: ')
+// let v = a*b*c
+// let s = 2*(a*b+b*c+a*c)
+// alert(`Объем параллелепипеда=${v}. Площадь его поверхности=${s} `)
+
+// let r = prompt('Радиус окружности:')
+// let pi = 3.14
+// let l = 2*pi*r
+// let s = pi*r**2
+// alert(`Длина окружности=${l}; площадь круга=${s}`)
+
+// let a = prompt('Число a=')
+// let b = prompt('Число b=')
+// let sr = (1*a+1*b)/2
+// alert(`Среднее арифметическое чисел=${sr}`)
+
+let a = prompt('Число a=')
+let b = prompt('Число b=')
+// function s(a, b){
+//     return (Math.sqrt(a + b);
+// }
+alert()
---
+++ main2.js
@@ -0,0 +1,11 @@
+let firstname = 'Ivan'
+let secondName = 'Morozov'
+let age = prompt('Назовите Ваш возраст')
+let fullName = firstname + secondName
+
+alert(firstname + ' ' + secondName)
+
+alert(`Привет, меня зовут: ${firstname} ${secondName}. Мой возраст: ${age} `)
+
+
+alert(45 + 55 * 4/16)
